d4ea8d4ee00a35c47c7ee4463a6761567b849d43
Martin Algañaraz <idcmardelplata@gmail.com>
2025-06-21 21:17:33 -0300

    build(lang): Change lenguage to typescript for better code organization

---
+++ src/server.js
@@ -0,0 +1,74 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ws_1 = require("ws");
+var server = new ws_1.WebSocket.Server({
+    port: 8080,
+    // verifyClient: client => {
+    //   const allowOrigins = ['wss://server.com', 'wss://chat.server.com'];
+    //   return allowOrigins.includes(client.origin);
+    // }
+});
+console.log("Server running at: 8080");
+var clients = [];
+var connections = {};
+server.on('connection', function (ws, req) {
+    // Limitar las conexiones por IP
+    var ip = req.socket.remoteAddress;
+    connections[ip] = (connections[ip] || 0) + 1;
+    if (connections[ip] > 2) {
+        ws.close(1008, "Limite de conexiones permitidas");
+        return;
+    }
+    console.log("Nuevo usuario conectado");
+    clients.push(ws);
+    ws.send(JSON.stringify({
+        type: 'system',
+        message: 'Bienvenido a virdevs chat!, escribe tus mensajes y presiona <enter>'
+    }));
+    broadcast({
+        type: 'system',
+        message: "Usuario se ha conectado. Total conectados ".concat(clients.length)
+    }, ws); // Excluye a este cliente
+    ws.on('message', function (data) {
+        try {
+            //parseamos el mensaje recibido
+            var mensaje = JSON.parse(data);
+            console.log("Mensaje recibido: ".concat(mensaje.texto));
+            // Enviar a todos los conectados
+            broadcast({
+                type: 'message',
+                texto: mensaje.texto,
+                timestamp: new Date().toLocaleTimeString()
+            });
+        }
+        catch (err) {
+            console.error("Error al procesar mensaje", err);
+        }
+    });
+    //cuando un cliente se desconecta
+    ws.on('close', function () {
+        console.log("Cliente desconectado");
+        var index = clients.indexOf(ws);
+        if (index > -1) {
+            clients.splice(index, 1);
+        }
+        connections[ip]--;
+        broadcast({
+            type: 'system',
+            message: "Usuario se ha desconectado. Total conectados ".concat(clients.length)
+        });
+        ws.on('error', function (error) {
+            console.error("Error en el websocket", error);
+        });
+    });
+    function broadcast(mensaje, excluir) {
+        if (excluir === void 0) { excluir = null; }
+        var msgJson = JSON.stringify(mensaje);
+        console.log(msgJson);
+        clients.forEach(function (client) {
+            if (client.readyState === ws_1.WebSocket.OPEN && client !== excluir) {
+                client.send(msgJson);
+            }
+        });
+    }
+});
