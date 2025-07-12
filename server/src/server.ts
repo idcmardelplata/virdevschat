52b18961031d1efd3be85bd8c71672d6d03bfacd
Martin Algañaraz <idcmardelplata@gmail.com>
2025-07-12 16:37:13 -0300

    build: add support for toggle flags in the server
    Added openfeature and flagd for toggle flags management and create a new
    flag called 'refactor-server' for starting server code refactoring in
    the local.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,43 +1,67 @@
 import { WebSocket } from 'ws';
 import ConnectionManager from './connectionManager';
+import { OpenFeature } from '@openfeature/server-sdk';
+import { FlagdProvider } from '@openfeature/flagd-provider';
+
+try {
+  OpenFeature.setProviderAndWait(new FlagdProvider());
+} catch (error) {
+  console.error(`Failed to initialize FlagdProvider: ${error}`);
+}
+
+const client = OpenFeature.getClient();
+const refactoring_server = client.getBooleanValue('refactor-server', true);
 
 
-const server = new WebSocket.Server({
-  port: 8080,
-});
-const manager = new ConnectionManager(2);
+refactoring_server.then(result => {
+  if (result) {
+    console.log(`Server en plena refactorizacion.`);
+    process.exit(0);
 
-console.log("Server running at: 8080");
+  } else {
+    const server = new WebSocket.Server({
+      port: 8080,
+    });
+    const manager = new ConnectionManager(2);
 
-server.on('connection', (ws, req) => {
-  let ip = req.socket.remoteAddress ?? "";
-  manager.addConnection(ws, ip);
+    console.log("Server running at: 8080");
 
-  ws.on('message', (data: any) => {
-    try {
-      const mensaje = JSON.parse(data);
-      console.log(`Mensaje recibido: ${mensaje.texto}`)
+    server.on('connection', (ws, req) => {
+      let ip = req.socket.remoteAddress ?? "";
+      manager.addConnection(ws, ip);
 
-      manager.broadcast({
-        type: 'message',
-        texto: mensaje.texto,
-        timestamp: new Date().toLocaleTimeString()
-      })
+      ws.on('message', (data: any) => {
+        try {
+          const mensaje = JSON.parse(data);
+          console.log(`Mensaje recibido: ${mensaje.texto}`)
 
-    } catch (err) {
-      console.error(`Error al procesar mensaje`, err);
-    }
+          manager.broadcast({
+            type: 'message',
+            texto: mensaje.texto,
+            timestamp: new Date().toLocaleTimeString()
+          })
 
-  });
+        } catch (err) {
+          console.error(`Error al procesar mensaje`, err);
+        }
 
-  ws.on('close', () => {
-    console.log(`Cliente desconectado`);
-    manager.remove(ws);
-  });
+      });
+
+      ws.on('close', () => {
+        console.log(`Cliente desconectado`);
+        manager.remove(ws);
+      });
 
 
-  ws.on('error', error => {
-    console.error(`Error en el websocket`, error);
-  });
+      ws.on('error', error => {
+        console.error(`Error en el websocket`, error);
+      });
 
-});
+    });
+
+  }
+
+
+
+})
+
